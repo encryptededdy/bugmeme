0f3aa7af4d49c4f1472a1da19b1835b75bad1111
encryptededdy <pcman2000@gmail.com>
2017-08-11 23:07:22 +1200

    I actually learned how to write java.

--- src/nz/zhang/Controller.java
+++ src/nz/zhang/Controller.java
@@ -5,7 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 import java.util.Random;
 
 public class Controller {
@@ -16,67 +16,28 @@ public class Controller {
     @FXML private CheckBox check5;
     @FXML
 
+    public static CheckBox randomBox(ArrayList<CheckBox> list) {
+        Random rand = new Random();
+        CheckBox box = list.get(rand.nextInt(list.size()));
+        return box;
+    }
+
+    @FXML
     protected void fixpressed(ActionEvent event) {
         System.out.println("fixing bugs");
-        check1.setSelected(false);
-        check2.setSelected(false);
-        check3.setSelected(false);
-        check4.setSelected(false);
-        check5.setSelected(false);
-        check1.setDisable(true);
-        check2.setDisable(true);
-        check3.setDisable(true);
-        check4.setDisable(true);
-        check5.setDisable(true);
-        for (int i = 0; i < (int)((Math.random()*5)+1); i++) {
-            int randBox = (int)((Math.random()*5)+1);
-            switch (randBox) {
-                case 1:
-                    System.out.println("setting check1");
-                    check1.setSelected(true);
-                    break;
-                case 2:
-                    System.out.println("setting check2");
-                    check2.setSelected(true);
-                    break;
-                case 3:
-                    System.out.println("setting check3");
-                    check3.setSelected(true);
-                    break;
-                case 4:
-                    System.out.println("setting check4");
-                    check4.setSelected(true);
-                    break;
-                case 5:
-                    System.out.println("setting check5");
-                    check5.setSelected(true);
-                    break;
-            }
+        ArrayList<CheckBox> checkBoxes = new ArrayList<>(Arrays.asList(check1, check2, check3, check4, check5));
+        for (CheckBox checkbox : checkBoxes) {
+            checkbox.setSelected(false);
+            checkbox.setDisable(true);
         }
-        for (int i = 0; i < (int)((Math.random()*5)+1); i++) {
-            int randBox = (int)((Math.random()*5)+1);
-            switch (randBox) {
-                case 1:
-                    System.out.println("enabling check1");
-                    check1.setDisable(false);
-                    break;
-                case 2:
-                    System.out.println("enabling check2");
-                    check2.setDisable(false);
-                    break;
-                case 3:
-                    System.out.println("enabling check3");
-                    check3.setDisable(false);
-                    break;
-                case 4:
-                    System.out.println("enabling check4");
-                    check4.setDisable(false);
-                    break;
-                case 5:
-                    System.out.println("enabling check5");
-                    check5.setDisable(false);
-                    break;
-            }
+        Random rand = new Random();
+        int randomSelections = rand.nextInt(5);
+        int randomEnables = rand.nextInt(5);
+        for (int i = 0; i < randomSelections; i++) {
+            randomBox(checkBoxes).setSelected(true);
+        }
+        for (int i = 0; i < randomEnables; i++) {
+            randomBox(checkBoxes).setDisable(false);
         }
 
 
